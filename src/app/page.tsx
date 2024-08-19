fea27c5765fb3400854c89076d39544e427f2c28
CaueAlvim <caue.eyti@gmail.com>
2024-08-19 15:10:27 -0300

    fix: utilizando switch para alterar estados da tela login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,16 +15,21 @@ import {
 import * as Yup from "yup";
 import { Anchor, Image, Text } from "@mantine/core";
 
-const Login = (props: any) => {
+function Login(props: any) {
   const { errors, values, handleChange, handleSubmit, handleBlur } = props;
-  const [register, setRegister] = useState(false);
+  const [currentState, setCurrentState] = useState('LOGIN');
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Container fluid display='flex' h='100vh' px={0} style={{ alignItems: 'center' }}>
-
-        {!register ? (
+  const renderState = () => {
+    switch (currentState) {
+      case 'LOGIN':
+        return (
           <>
+            <Image
+              fit='fill'
+              w='auto'
+              maw='60%'
+              h='100%'
+              src="/loginImage.png" />
             <Paper radius={0} px={30} w='40%'>
 
               <Title order={2} ta="center" mt="md" mb={50}>
@@ -63,21 +68,16 @@ const Login = (props: any) => {
 
               <Text ta="center" mt="md">
                 Não possui conta?{' '}
-                <Anchor href="#" fw={700} onClick={() => setRegister(true)}>
+                <Anchor href="#" fw={700} onClick={() => setCurrentState('REGISTRO')}>
                   Registre-se
                 </Anchor>
               </Text>
 
             </Paper>
-
-            <Image
-              fit='fill'
-              w='auto'
-              maw='60%'
-              h='100%'
-              src="/loginImage.png" />
           </>
-        ) : (
+        );
+      case 'REGISTRO':
+        return (
           <>
             <Image
               fit='fill'
@@ -162,24 +162,160 @@ const Login = (props: any) => {
                 />
               </Container>
 
-
-              <Button fullWidth mt="xl" size="md" type="submit">
+              <Button fullWidth mt="xl" size="md" onClick={() => setCurrentState('CONTINUARREGISTRO')}>
                 CONTINUAR REGISTRO
               </Button>
 
               <Text ta="center" mt="md">
                 Já possui conta?{' '}
-                <Anchor href="#" fw={700} onClick={() => setRegister(false)}>
+                <Anchor href="#" fw={700} onClick={() => setCurrentState('LOGIN')}>
                   Entrar
                 </Anchor>
               </Text>
 
             </Paper>
           </>
-        )}
+        );
+      case 'CONTINUARREGISTRO':
+        return (
+          <>
+            <Image
+              fit='fill'
+              w='auto'
+              maw='60%'
+              h='100%'
+              src="/loginImage.png" />
 
+            <Paper radius={0} px={30} w='40%'>
+
+              <Title order={2} ta="center" mt="md" mb={50}>
+                Concluir registro
+              </Title>
+
+              <Input
+                name="registroDiretorResponsavel"
+                value={values.registroDiretorResponsavel}
+                label="Diretor responsável"
+                placeholder="Diretor responsável"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                validate={errors.registroDiretorResponsavel}
+                required
+                type={undefined}
+                sufixComponent={undefined}
+              />
+
+              <Input
+                name="registroNomeEscola"
+                value={values.registroNomeEscola}
+                label="Nome da Escola"
+                placeholder="Nome da Escola"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                validate={errors.registroNomeEscola}
+                required
+                type={undefined}
+                sufixComponent={undefined}
+              />
+
+              <Input
+                name="registroRua"
+                value={values.registroRua}
+                label="Rua"
+                placeholder="Rua"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                validate={errors.registroRua}
+                required
+                type={undefined}
+                sufixComponent={undefined}
+              />
+
+              <Container display='flex' px={0} style={{ gap: '1rem' }}>
+                <Input
+                  name="registroBairro"
+                  value={values.registroBairro}
+                  label="Bairro"
+                  placeholder="Bairro"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  validate={errors.registroBairro}
+                  required
+                  type={undefined}
+                  sufixComponent={undefined}
+                  style={{ width: '50%' }}
+                />
+
+                <Input
+                  name="registroComplemento"
+                  value={values.registroSenhaConfirmar}
+                  label="Confirmar Senha"
+                  placeholder="Confirmar Senha"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  validate={errors.registroSenhaConfirmar}
+                  required
+                  type={undefined}
+                  sufixComponent={undefined}
+                  style={{ width: '50%' }}
+
+                />
+              </Container>
+
+              <Container display='flex' px={0} style={{ gap: '1rem' }}>
+                <Input
+                  name="registroNumero"
+                  value={values.registroNumero}
+                  label="Número"
+                  placeholder="Número"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  validate={errors.registroNumero}
+                  required
+                  type={undefined}
+                  sufixComponent={undefined}
+                  style={{ width: '50%' }}
+                />
+
+                <Input
+                  name="registroCEP"
+                  value={values.registroCEP}
+                  label="CEP"
+                  placeholder="CEP"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  validate={errors.registroCEP}
+                  required
+                  type={undefined}
+                  sufixComponent={undefined}
+                  style={{ width: '50%' }}
+
+                />
+              </Container>
+
+              <Button fullWidth mt="xl" size="md" type="submit">
+                FINALIZAR REGISTRO
+              </Button>
+
+              <Text ta="center" mt="md">
+                Já possui conta?{' '}
+                <Anchor href="#" fw={700} onClick={() => setCurrentState('LOGIN')}>
+                  Entrar
+                </Anchor>
+              </Text>
+
+            </Paper>
+          </>
+        );
+    }
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Container fluid display='flex' h='100vh' px={0} style={{ alignItems: 'center' }}>
+        {renderState()}
       </Container>
-    </Form>
+    </Form >
   );
 };
 
